d5f81fbfc394df4764e7edca57577a5be5f6bf03
= <trandoducnghia@gmail.com>
2024-03-10 20:58:10 +0700

    [Update] create method UpdateChild, using create new child ease.

--- Assets/script/ChildControllers.cs
+++ Assets/script/ChildControllers.cs
@@ -1,9 +1,31 @@
-﻿using UnityEngine;
+﻿using DG.Tweening;
+using TMPro;
+using UnityEngine;
 
 namespace script
 {
     public class ChildControllers : MonoBehaviour
     {
         private float _speed = 1f;
+
+        [SerializeField] private GameObject images;
+        [SerializeField] private TextMeshProUGUI text;
+        private Ease _ease = Ease.Linear;
+
+        public void UpdateEase(Ease t)
+        {
+            _ease = t;
+        }
+        public void Run()
+        {
+            images.transform.DOLocalMoveY(-104, _speed).SetEase(_ease).SetLoops(-1, LoopType.Yoyo);
+            text.text = _ease.ToString();
+        }
+
+        private void Start()
+        {
+            Run();
+        }
+
     }
 }
--- Assets/script/MainController.cs
+++ Assets/script/MainController.cs
@@ -1,8 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using script;
 using UnityEngine;
-
+using DG.Tweening;
 public class MainController : MonoBehaviour
 {
     private List<ChildControllers> _list = new List<ChildControllers>();
@@ -12,6 +13,17 @@ public class MainController : MonoBehaviour
     void Start()
     {
         
+    }
+
+    public void UpdateChild()
+    {
+        foreach (Ease t in Enum.GetValues(typeof(Ease)))
+        {
+            var child = Instantiate(childControllers, transform);
+            child.UpdateEase(t);
+            child.Run();
+            _list.Add(child);
+        }
     }
 
     // Update is called once per frame
